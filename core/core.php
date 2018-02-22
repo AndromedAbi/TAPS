052769f9c431ca8bbff87f73b695cdf2983d440a
AndromedAbi <abisilvap@gmail.com>
2018-02-22 15:34:33 -0400

    con nueva estructura
    :banana:

---
+++ core/core.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  EL NUCLUEO DE LA APLICACIÓN
+*/
+define('HTML_DIR','html/');
+define('APP_TITLE','Taps Diseños');
+define('APP_COPY','Copyright &copy; ' . date('Y',time()) . ' Taps Diseños. ');
+define('APP_URL','http://localhost/startbootstrap-shop-homepage-gh-pages');
+
+require('vendor/autoload.php');
+?>
---
+++ html/overall/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <base href="<?php APP_URL ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.6.5, mobirise.com">
+  <link rel="shortcut icon" href="views/images/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:700,400&amp;subset=cyrillic,latin,greek,vietnamese">
+  <link rel="stylesheet" href="views/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="views/bootstrap/css/main.css">
+  <link rel="stylesheet" href="views/socicon/css/socicon.min.css">
+  <link rel="stylesheet" href="views/mobirise/css/style.css">
+
+
+  <link rel="stylesheet" href="views/bootstrap/css/business-casual.css">
+
+<script src="views/apps/js/generales.js"></script>
+  <title><?php echo APP_TITLE; ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/shop-homepage.css" rel="stylesheet">
+
+  </head>
