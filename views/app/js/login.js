e527b9525999c5516bba31a750480ba5f5840cb4
plagajesus <32114988+plagajesus@users.noreply.github.com>
2018-02-23 22:52:10 -0400

    asd

--- views/app/js/login.js
+++ views/app/js/login.js
@@ -1,9 +1,11 @@
-function runScriptLogin(event){
+function runScriptLogin(event)
+{
     // si la tecla presionada es igual al número ASCI de la tecla ENTER
-    if(event.keyCode == 13){
+    if(event.keyCode == 13)
+    {
       goLogin();
     }
-  }
+ }
 function goLogin()
 {
     var connect, form , response, result;
@@ -11,15 +13,36 @@ function goLogin()
     connect = window.XMLHttpResquest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     connect.onreadystatechange = function()
     {
-      if(connect.readyState == 4 && connect.status == 200)
-      {
-
-      }
-      else if (connect.readyState !=4)
-      {
+        if(connect.readyState == 4 && connect.status == 200)
+        {
+            if(connect.responseText == 1)
+            {
+              result = '<div class="alert alert-dismissible alert-warning">';
+              result +='<button type="button" class="close" data-dismiss="alert">&times;</button>';
+              result +='<h4 class="alert-heading">Bienvedios a Taps Diseños </h4>';
+              result +='<p class="mb-0">Ha Iniciado Conrrectamente';            
+              result +='<a href="#" class="alert-link">vel scelerisque nisl consectetur et</a>.</p>';
+              result +='</div>';
+              __('_ajax_login_').innerHTML = result;
+              location.reload();
+            }
+            else
+            {
+              __('_ajax_login_').innerHTML = connect.responseText;
+            }
+        }
+        else if (connect.readyState != 4)
+        {
+          result = '<div class="alert alert-dismissible alert-warning">';
+          result +='<button type="button" class="close" data-dismiss="alert">&times;</button>';
+          result +='<h4 class="alert-heading">Procesando...</h4>';
+          result +='<p class="mb-0"> Estamos Iniciando Session Por favor espere...';            
+          result +='<a href="#" class="alert-link">vel scelerisque nisl consectetur et</a>.</p>';
+          result +='</div>';
         
-      }
-    }
+          __('_ajax_login_').innerHTML = result;
+        }
+     }
     connect.open('POST','ajax.php?mode=login',true);
     connect.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
     connect.send(form);
